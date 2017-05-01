99538ad2cf71b990909ac2228f7544b6636caef3
hashrock <hashedrock@gmail.com>
2017-05-02 00:31:35 +0900

    refactor(convert): switch文を使ったリファクタリングを実施致しました。

--- bundle.js
+++ bundle.js
@@ -6,62 +6,106 @@ var traverse = require("txt-ast-traverse").traverse;
 var VisitorOption = require("txt-ast-traverse").VisitorOption;
 
 function convert(input) {
-  var output = ""
+  var output = []
   var AST = parse(input);
 
   traverse(AST, {
     enter(node) {
       //console.log("enter", node.type);
-      if (node.type === Syntax.Paragraph) {
-        output += kaigyo(node.children.map(item => item.value).join("")) + "\n"
-        output += "\n"
-        return VisitorOption.Skip;
-      }
+      switch (node.type) {
+        case Syntax.Document:
+          break;
+        case Syntax.Paragraph:
+          output.push(kaigyo(node.children.map(item => item.value).join("")))
+          output.push("")
+          return VisitorOption.Skip;
+          break;
 
-      if (node.type === Syntax.HorizontalRule) {
-        output += "----------------------------------------"
-        output += "\n"
-      }
+        case Syntax.BlockQuote:
+          break;
 
-      if (node.type === Syntax.Header) {
-        output += "\n"
-        if (node.depth === 1) {
-          output += "【" + node.children.map(item => item.value).join("") + "】" + "\n"
-        }
-        if (node.depth >= 2) {
-          output += "■" + node.children.map(item => item.value).join("") + "" + "\n"
-        }
-        output += "\n"
-        return VisitorOption.Skip;
-      }
-      if (node.type === Syntax.List) {
-        var toText = item => item.value
-        if(node.ordered){
-          var toItem = (listitem, i) => "　（"+(i+1)+"）"+ listitem.children[0].children.map(toText).join("") + "\n"
-          var listtext = node.children.map(toItem).join("")
-          output += listtext
-        }else{
-          var toItem = listitem => "　・"+ listitem.children[0].children.map(toText).join("") + "\n"
-          var listtext = node.children.map(toItem).join("")
-          output += listtext
-        }
-        output += "\n"
-        return VisitorOption.Skip;
-      }
-      if (node.type === Syntax.ListItem) {
+        case Syntax.ListItem:
+          break;
+
+        case Syntax.List:
+          var toText = item => item.value
+          if (node.ordered) {
+            var toItem = (listitem, i) => "　（" + (i + 1) + "）" + listitem.children[0].children.map(toText).join("") + "\n"
+            var listtext = node.children.map(toItem).join("")
+            output.push(listtext)
+          } else {
+            var toItem = listitem => "　・" + listitem.children[0].children.map(toText).join("") + "\n"
+            var listtext = node.children.map(toItem).join("")
+            output.push(listtext)
+          }
+          return VisitorOption.Skip;
+          break;
+
+        case Syntax.Header:
+          output.push("")
+          if (node.depth === 1) {
+            output.push("【" + node.children.map(item => item.value).join("") + "】")
+          }
+          if (node.depth >= 2) {
+            output.push("■" + node.children.map(item => item.value).join("") + "")
+          }
+          output.push("")
+          return VisitorOption.Skip;
+
+          break;
+
+        case Syntax.CodeBlock:
+          break;
+
+        case Syntax.Html:
+          break;
+
+        case Syntax.ReferenceDef:
+          break;
+
+        case Syntax.HorizontalRule:
+          output.push("----------------------------------------")
+          break;
+
+        case Syntax.Str:
+          break;
+
+        case Syntax.Break:
+          break;
+
+        case Syntax.Emphasis:
+          break;
+
+        case Syntax.Strong:
+          break;
+
+        case Syntax.Html:
+          break;
+
+        case Syntax.Link:
+          break;
+
+        case Syntax.Image:
+          break;
+
+        case Syntax.Code:
+          break;
+
+        default:
+          break;
       }
     },
     leave(node) {
       //console.log("leave", node.type);
     }
   });
-  return output
+  return output.join("\n")
 }
 
 var $input = document.querySelector("#input")
 var $output = document.querySelector("#output")
 
-setInterval(function(){
+setInterval(function () {
   $output.value = convert($input.value)
 }, 1000)
 
