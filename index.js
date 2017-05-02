450e204892dfd0f18f88812ab7ccff6c67782844
hashrock <hashedrock@gmail.com>
2017-05-02 20:14:01 +0900

    fix(convert): パラグラフ内のインライン要素がレンダリングされない不具合の修正

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-var kaigyo = require("./lib/kaigyo")
-var parse = require("markdown-to-ast").parse;
-var Syntax = require("markdown-to-ast").Syntax;
-var traverse = require("txt-ast-traverse").traverse;
-var VisitorOption = require("txt-ast-traverse").VisitorOption;
-
 var $input = document.querySelector("#input")
 var $output = document.querySelector("#output")
 var convert = require("./lib/convert")
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var tape = require("tape")
 var kaigyo = require("./lib/kaigyo")
+var convert = require("./lib/convert")
 
 tape("改行", function(t){
   t.plan(3)
@@ -15,4 +16,49 @@ tape("改行", function(t){
   input = "山田さん".repeat(10)
   t.equal(kaigyo(input), "山田さん山田さん山田さん山田さん山田さん山田さん山田さん山田さん山田\nさん山田さん");
 
+})
+
+tape("markdown: 見出し１", function(t){
+  t.plan(1)
+let input = `# タイトル
+
+本文本文
+`
+
+let expected = `
+【タイトル】
+
+本文本文
+`
+  t.equals(convert(input), expected)
+})
+
+
+tape("markdown: 見出し２", function(t){
+  t.plan(1)
+let input = `## タイトル
+
+本文本文
+`
+
+let expected = `
+■タイトル
+
+本文本文
+`
+  t.equals(convert(input), expected)
+})
+
+tape("markdown: 強調（何もしない）", function(t){
+  t.plan(1)
+  let input = `ここを*強調*したい`
+  let expected = `ここを強調したい\n`
+  t.equals(convert(input), expected)
+})
+
+tape("markdown: リンク（何もしない）", function(t){
+  t.plan(1)
+  let input = `詳しくは、[こちら](http://example.com)を参照して下さい`
+  let expected = `詳しくは、こちらを参照して下さい\n`
+  t.equals(convert(input), expected)
 })
